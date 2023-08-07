0993470c81d1c18a5486f8eed6f29f9718d12882
WL4949 <66366717+WL4949@users.noreply.github.com>
2023-08-06 21:36:17 -0500

    Update views.py

--- movie/views.py
+++ movie/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'Jean P'})
+    return render(request, 'home.html', {'name':'Jean Paul.'})
     
